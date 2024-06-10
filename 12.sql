b7daf2d1c84393782e8471f91df2bee4b12e6460
Manu Krishna <krishnamanu27112004@gmail.com>
2024-06-10 21:53:10 +0530

    Update 12.sql

--- 12.sql
+++ 12.sql
@@ -3,11 +3,12 @@ empno VARCHAR(5),
 ename VARCHAR(10),
 dept VARCHAR(7), 
 salary INT);
-INSERT INTO emp_salary VALUES('101','Arun', 'Sales', 10000);
-INSERT INTO emp_salary VALUES('102', 'Amal', 'HR', 15000);
-INSERT INTO emp_salary VALUES('103', 'Arya', 'Sales', 2000);
-INSERT INTO emp_salary VALUES('104', 'Sandra', 'HR', 13000);
-INSERT INTO emp_salary VALUES('105', 'Anil', 'Sales', 14000);
+INSERT INTO emp_salary VALUES
+  ('101','Arun', 'Sales', 10000),
+  ('102', 'Amal', 'HR', 15000),
+  ('103', 'Arya', 'Sales', 2000),
+  ('104', 'Sandra', 'HR', 13000),
+  ('105', 'Anil', 'Sales', 14000);
 CREATE OR REPLACE funtion avg_sal_sales() RETURNS INTEGER AS $$
 DECLARE
 avg_salary INTEGER;
