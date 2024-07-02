93707456b177faf7eef3dbdd911d5b3e7f478311
Manu Krishna <krishnamanu27112004@gmail.com>
2024-07-02 21:31:07 +0530

    Update 10.sql

--- 10.sql
+++ 10.sql
@@ -1,28 +1,23 @@
-CREATE OR REPLACE FUNCTION get_fibonacci(limit_val INTEGER)
-RETURNS TABLE(fib_number INTEGER) AS
-$$
+CREATE OR REPLACE FUNCTION print_fibonacci(limit_val INTEGER)
+RETURNS VOID AS $$
 DECLARE
-a INTEGER := 0; 
-b INTEGER := 1; 
+a INTEGER := 0;
+b INTEGER := 1;
 temp INTEGER;
 BEGIN
 IF limit_val < 1 THEN
-RAISE EXCEPTION 'Limit should be greater than or equal to 1'; 
-END IF; 
-
-RETURN QUERY SELECT a;
-
-IF limit_val >= 2 THEN 
-RETURN QUERY SELECT b;
+RAISE EXCEPTION 'limit should be greater than or equal to one';
 END IF;
-
-WHILE (a + b) <= limit_val LOOP 
+RAISE NOTICE '%', a;
+IF limit_val >= 2 THEN
+RAISE NOTICE '%', b;
+END IF;
+WHILE (a + b) <= limit_val LOOP
 temp := a + b;
-RETURN QUERY SELECT temp; 
+RAISE NOTICE '%', temp;
 a := b;
 b := temp;
-END LOOP; 
+END LOOP;
 END;
 $$ LANGUAGE plpgsql;
-
-SELECT * FROM get_fibonacci(100);
+select print_fibonacci(100);
